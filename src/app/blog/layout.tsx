528d3c3a89dd99fc50d92cf5713eb82cf7586e6f
Jonas <jonaslutz@t-online.de>
2025-12-29 15:27:46 +0100

    noindex for blog

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,6 +1,11 @@
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
 
+export const metadata = {
+  title: 'Blog - Fairfield & Archer',
+  robots: 'noindex,nofollow',
+};
+
 const BlogLayout = ({ children }: { children: React.ReactNode }) => (
   <div>
     <Header />
